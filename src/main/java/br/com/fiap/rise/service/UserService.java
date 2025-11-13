56402937eba330c8d2e5f473f307f974c92b81df
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 18:39:58 -0300

    Aplicando boas praticas na injecao e removendo imports nao usados

--- src/main/java/br/com/fiap/rise/service/UserService.java
+++ src/main/java/br/com/fiap/rise/service/UserService.java
@@ -3,16 +3,17 @@ package br.com.fiap.rise.service;
 import br.com.fiap.rise.dto.UserDTO;
 import br.com.fiap.rise.model.User;
 import br.com.fiap.rise.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.UUID;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public UserDTO findById(UUID id) {
         User user = userRepository.findById(id)
