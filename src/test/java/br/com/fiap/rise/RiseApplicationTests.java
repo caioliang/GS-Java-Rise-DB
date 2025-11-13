0e330eaf3ec2711ac6d3fa41d17dd3b7a0c503f1
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 16:16:52 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/fiap/rise/RiseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.rise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
