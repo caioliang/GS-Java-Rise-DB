b9f64874575b6c3ab8588240bb0196b1e0af124e
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 18:37:55 -0300

    Aplicando boas praticas na injecao

--- src/main/java/br/com/fiap/rise/controller/UserController.java
+++ src/main/java/br/com/fiap/rise/controller/UserController.java
@@ -14,8 +14,11 @@ import java.util.UUID;
 @RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable UUID id) {
