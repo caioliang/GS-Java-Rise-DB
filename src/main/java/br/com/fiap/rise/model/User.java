7df64cacf233cd46fd79be9ebfe4e0c221e77f12
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 17:25:22 -0300

    Adicionando model de usuario

--- src/main/java/br/com/fiap/rise/model/User.java
+++ src/main/java/br/com/fiap/rise/model/User.java
@@ -1,4 +1,40 @@
 package br.com.fiap.rise.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_USER")
 public class User {
+
+    @Id
+    private final UUID id = UUID.randomUUID();
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @NotBlank
+    @Email
+    @Size(max = 150)
+    private String email;
+
+    @NotBlank
+    @Size(min = 8)
+    private String password;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{11}")
+    private String cpf;
+
+    @NotBlank
+    @Past
+    private LocalDate birthDate;
 }
