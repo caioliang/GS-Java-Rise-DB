345b510617fea66ad63f6a39ee083121fa167e09
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 22:53:05 -0300

    Importando List em DTO de curriculo e removendo mensagens

--- src/main/java/br/com/fiap/rise/dto/ResumeDTO.java
+++ src/main/java/br/com/fiap/rise/dto/ResumeDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -13,10 +14,10 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ResumeDTO {
 
-    @NotBlank(message = "Objective is required.")
+    @NotBlank
     private String objective;
 
-    @NotNull(message = "User ID is required to link the resume.")
+    @NotNull
     private UUID userId;
 
     private List<WorkExperienceDTO> workExperiences;
