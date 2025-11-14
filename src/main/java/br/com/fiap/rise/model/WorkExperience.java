556930ee96b185141b3a31721c64f17fc3523092
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 22:13:21 -0300

    Melhorando validacao em model e DTO

--- src/main/java/br/com/fiap/rise/model/WorkExperience.java
+++ src/main/java/br/com/fiap/rise/model/WorkExperience.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -17,9 +18,11 @@ public class WorkExperience {
     private UUID id = UUID.randomUUID();
 
     @NotBlank
+    @Length(min = 3, max = 100)
     private String company;
 
     @NotBlank
+    @Length(min = 3, max = 100)
     private String role;
 
     @NotNull
