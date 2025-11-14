d152aff756d7aecbaa550c7b97fb6e2a6ee3e9ab
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 21:39:37 -0300

    Adicionando metodos no repositorio de experiencas profissionais

--- src/main/java/br/com/fiap/rise/repository/WorkExperienceRepository.java
+++ src/main/java/br/com/fiap/rise/repository/WorkExperienceRepository.java
@@ -4,8 +4,10 @@ import br.com.fiap.rise.model.WorkExperience;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WorkExperienceRepository extends JpaRepository<WorkExperience, UUID> {
+    Optional<WorkExperience> findById(UUID id);
     List<WorkExperience> findByResumeId(UUID resumeId);
 }
