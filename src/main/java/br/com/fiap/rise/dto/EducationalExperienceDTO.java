a7a09023a84d0e3e6b7a91733e4f95f420d92ad8
allanbm100 <allan.bm.100@gmail.com>
2025-11-13 22:09:50 -0300

    Criando DTO de experiencia academica

---
+++ src/main/java/br/com/fiap/rise/dto/EducationalExperienceDTO.java
@@ -0,0 +1,39 @@
+package br.com.fiap.rise.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EducationalExperienceDTO {
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private UUID resumeId;
+
+    @NotBlank
+    @Length(min = 3, max = 100)
+    private String institution;
+
+    @NotBlank
+    @Length(min = 3, max = 100)
+    private String course;
+
+    @NotNull
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @NotBlank
+    private String description;
+
+}
