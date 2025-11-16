f4c887a9382919e7c07115a748c8a3ff5eb8fc80
allanbm100 <allan.bm.100@gmail.com>
2025-11-16 19:48:19 -0300

    Adicionando implementacao de UserDetails na model de autenticacao

--- src/main/java/br/com/fiap/rise/model/Auth.java
+++ src/main/java/br/com/fiap/rise/model/Auth.java
@@ -5,13 +5,17 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @Entity
 @Table(name = "TB_AUTH")
-public class Auth {
+public class Auth implements UserDetails {
 
     @Id
     private UUID id = UUID.randomUUID();
@@ -22,11 +26,46 @@ public class Auth {
     private String email;
 
     @NotBlank
-    @Size(min = 8)
+    @Size(min = 60)
     private String password;
 
     @OneToOne(fetch = FetchType.LAZY)
     @MapsId
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
