ec0d8e991533ee3806523be1b626174813b5aa15
allanbm100 <allan.bm.100@gmail.com>
2025-11-16 21:56:06 -0300

    Corrigindo issue de aplicacao rotativa

--- src/main/java/br/com/fiap/rise/config/AuthFilter.java
+++ src/main/java/br/com/fiap/rise/config/AuthFilter.java
@@ -6,12 +6,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class AuthFilter extends OncePerRequestFilter {
@@ -19,7 +21,7 @@ public class AuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final AuthService authService;
 
-    public AuthFilter(JwtService jwtService, AuthService authService) {
+    public AuthFilter(JwtService jwtService, @Lazy AuthService authService) {
         this.jwtService = jwtService;
         this.authService = authService;
     }
@@ -32,8 +34,8 @@ public class AuthFilter extends OncePerRequestFilter {
             try {
                 var jwt = header.replace("Bearer ", "");
 
-                var userIdString = jwtService.validateAndExtractUserId(jwt);
-                var userDetails = authService.loadUserByUsername(userIdString);
+                var userId = jwtService.validateAndExtractUserId(jwt);
+                var userDetails = authService.loadUserById(UUID.fromString(userId));
 
                 var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 SecurityContextHolder.getContext().setAuthentication(authentication);
