df9d5ba62a1051b5e5e38d9ab070a0a090835955
Allanbm100 <allan.bm.100@gmail.com>
2025-11-23 11:48:48 -0300

    Corrigindo nomes de InsightService

--- src/main/java/br/com/fiap/rise/controller/InsightController.java
+++ src/main/java/br/com/fiap/rise/controller/InsightController.java
@@ -2,7 +2,7 @@ package br.com.fiap.rise.controller;
 
 import br.com.fiap.rise.dto.InsightDTO;
 import br.com.fiap.rise.dto.ResumeDTO;
-import br.com.fiap.rise.service.AiInsightService;
+import br.com.fiap.rise.service.InsightService;
 import br.com.fiap.rise.service.ResumeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,21 +14,21 @@ import java.util.UUID;
 public class InsightController {
 
     private final ResumeService resumeService;
-    private final AiInsightService aiInsightService;
+    private final InsightService insightService;
 
-    public InsightController(ResumeService resumeService, AiInsightService aiInsightService) {
+    public InsightController(ResumeService resumeService, InsightService insightService) {
         this.resumeService = resumeService;
-        this.aiInsightService = aiInsightService;
+        this.insightService = insightService;
     }
 
     @GetMapping
     public ResponseEntity<InsightDTO> getInsights(@PathVariable UUID userId, @RequestParam(required = false, defaultValue = "false") boolean cached) {
         ResumeDTO resume = resumeService.findByUserId(userId);
         if (cached) {
-            InsightDTO cachedResp = aiInsightService.getLastCachedForResume(resume);
+            InsightDTO cachedResp = insightService.getLastCachedForResume(resume);
             if (cachedResp != null) return ResponseEntity.ok(cachedResp);
         }
-        return ResponseEntity.ok(aiInsightService.generateInsights(resume));
+        return ResponseEntity.ok(insightService.generateInsights(resume));
     }
 }
 
